988a53842e756c732b2019ac9598ac6f82f72548
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-12 18:21:44 +0200

    --- Auto-Git Commit ---

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,18 @@ function defaultTask(done) {
 
 gulp.task('minify-css', function (done) {
     return gulp.src('src/css/*.css')
-
     .pipe(cleanCSS({
         compatibility: 'ie8'
     }))
-
     .pipe(gulp.dest('dist/css/'))
+    done();
+})
 
+gulp.task('minify-js', function (done) {
+    return gulp.src('src/css/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('dist/css/'))
     done();
 })
