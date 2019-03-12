7ca310072a6c6144cb2fa23adaeacca01d5a49cb
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-12 20:30:31 +0200

    --- Auto-Git Commit ---

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
+var tinyPNG = require('gulp-tinypng-compress');
 
 gulp.task('default', defaultTask);
 
@@ -25,10 +26,25 @@ gulp.task('move-js', function (done) {
 })
 
 gulp.task('htmlmin', function (done) {
-    return gulp.src('.html')
+    return gulp.src('*.html')
         .pipe(htmlmin({ 
             collapseWhitespace: true 
         }))
         .pipe(gulp.dest('dist/'))
     done();
 })
+
+gulp.task('fonts', function (done) {
+    return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+    done();
+})
+
+gulp.task('tinyPNG', function (done) {
+    return gulp.src('src/img//**/*.{png,jpg,jpeg}')
+        .pipe(tinyPNG({
+            key: 'fFm7ZvwblMprMRCwWGjlz4hHRGP5HMgy'
+        }))
+        .pipe(gulp.dest('dist/img/'));
+    done();
+});
